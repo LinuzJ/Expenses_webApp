b6ec685ad916a20d970f35a225a2abbdd7921198
LinuzJ <linusjern00@gmail.com>
2021-01-07 17:36:23 +0200

    Update to the Add page

--- pages/add.js
+++ pages/add.js
@@ -9,6 +9,7 @@ import {
   Stack,
   Select,
   Divider,
+  Text,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
@@ -17,6 +18,9 @@ import AlertMessageFail from "../components/AlertMessageFail";
 
 export default function add() {
   const { register, handleSubmit, errors } = useForm();
+
+  const [value, setValue] = useState("");
+  const handleChange = (event) => setValue(event.target.value);
 
   const onSubmit = (data) => {
     fetch("http://localhost:3000/api/addToExpenses", {
@@ -44,49 +48,64 @@ export default function add() {
     <Layout>
       <Flex
         flexDirection="column"
-        alignItems="center"
+        alignItems="stretch"
+        alignContent="space-evenly"
         justifyContent="center"
-        p="20px"
+        p="30px"
+        m="30px"
       >
-        <form
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-          onSubmit={handleSubmit(onSubmit)}
-          margin="auto"
-          width="70%"
-          border="black"
-          p="20px"
-        >
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Select
             placeholder="Select payer"
             type="text"
             name="user"
+            m="10px"
             ref={register({ required: true })}
+            height="50px"
           >
             <option value="Calle">Calle</option>
             <option value="Linus">Linus</option>
           </Select>
-          <input
+          <Input
+            height="50px"
             type="text"
             placeholder="Description"
             name="what"
+            m="10px"
+            size="nm"
             ref={register({
               required: "Tell what the expense is!",
             })}
           />
-          <input
-            type="number"
+          <Text m="10px">
+            Value: {value}
+            {value !== "" && " €"}
+          </Text>
+          <Input
+            height="50px"
+            value={value}
+            onChange={handleChange}
             placeholder="Enter amount"
+            type="number"
             name="amount"
+            m="10px"
+            size="sm"
             ref={register({
               required: "You have to enter an amount!",
             })}
           />
-          {errors.amount && <p>{errors.amount.message}</p>}
+
           {errors.what && <p>{errors.what.message}</p>}
-          <input type="submit" />
+          {errors.amount && <p>{errors.amount.message}</p>}
+          <Button
+            type="submit"
+            m="10px"
+            rightIcon={"➞"}
+            colorScheme="teal"
+            variant="outline"
+          >
+            Add expense
+          </Button>
         </form>
         {resultOfPost !== "" && resultOfPost}
         <Divider p="20px" m="20px" />
--- pages/api/addToExpenses.js
+++ pages/api/addToExpenses.js
@@ -4,13 +4,13 @@ import sqlite3 from "sqlite3";
 export default async function handler(req, res) {
   const user = req.body.user;
   const amount = req.body.amount;
-  const desription = req.body.what;
+  const description = req.body.what;
   const isDeleted = false;
-
+  console.log(user);
   const db = await open({ filename: "expenses.db", driver: sqlite3.Database });
   await db.run(
-    `INSERT INTO expenses (user, what, amount, deleted) VALUES("${user}", "${desription}", ${amount}, ${isDeleted})`
-    // (user, desription, amount, isNotDeleted)
+    `INSERT INTO expenses (user, what, amount, deleted) VALUES("${user}", "${description}", ${amount}, ${isDeleted})`
+    // (user, desription, amount, isDeleted)
   );
   res.end();
 }
